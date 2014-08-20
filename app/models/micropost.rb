6561af24dfe06bd305ab0a4bfb6729938d8397d4
Gera Gulina <gera-gulina@yandex.kz>
2014-08-20 15:20:56 +0000

    Add user microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ActiveRecord::Base
-  validates :content, length: {maximum: 140, minimum: 1}
-  
+  validates :user_id, presence: true
+  validates :content, length: {maximum:140, minimum:1}
   belongs_to :user
-  
+  default_scope -> { order('created_at DESC')}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :sessions,   only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
+
+  
   get 'users/show'
 
   resources :users
@@ -12,11 +21,7 @@ Rails.application.routes.draw do
   match '/help', to: 'static_pages#help', via: :get
   match '/about', to: 'static_pages#about', via: :get
   match '/contact', to: 'static_pages#contact', via: :get
-  
-
-  resources :microposts
-
-  resources :users
+   
   
   
   # The priority is based upon order of creation: first created -> highest priority.
